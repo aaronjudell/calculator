7d3f311920ab904a5b840037aef9c8f9657e3f5f
aaronjudell <ajudell@gmail.com>
2023-07-05 13:45:12 -0400

    Add Basic Math Functions

---
+++ script.js
@@ -0,0 +1,42 @@
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    if (b == 0) {
+        return "Cannot Divide by 0";
+    }
+    return a / b;
+}
+
+let firstNum = 0;
+let secondNum = 0;
+let operator = 0;
+
+function operate(firstNum, secondNum, operator) {
+    switch (operator) {
+        case '+':
+            return add(firstNum, secondNum);
+            break;
+        case '-':
+            return subtract(firstNum, secondNum);
+            break;
+        case '*':
+            return multiply(firstNum, secondNum);
+            break;
+        case '/':
+            return divide(firstNum, secondNum);
+            break;
+        default:
+            return 'ERROR improper operator chosen';
+            break;
+    }
+}
